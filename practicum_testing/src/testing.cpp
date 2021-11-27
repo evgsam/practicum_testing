a3c854cffcbac14841c161c1e4bbde0d769d4429
evgsam <evgsamarin@gmail.com>
2021-11-27 22:29:32 +0300

    Учим фрейморк проерять элементы контейнеров

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,32 +1,153 @@
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <map>
+#include <set>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-
-void AssertImpl(bool value, const string& t_str,  const string& file,
-                     const string& func, unsigned line, const string& hint) {
-   if (!value){
-       cout << boolalpha;
-       cout << file << "("s << line << "): "s << func << ": "s;
-       cout << "ASSERT("s << t_str << ") failed."s;
-       if (!hint.empty()) {
-           cout << " Hint: "s << hint;
-       }
-       cout << endl;
-       abort();
-
-   }
+template <typename Elements1, typename Elements2>
+ostream& operator<<(ostream& out, const pair<Elements1, Elements2>& stream) {
+    out <<stream.first<< ": "s<<stream.second;
+    return out;
 }
 
-#define ASSERT(expr) AssertImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, ""s)/* реализовать самостоятельно */
+template <typename Elements>
+ostream& Print(ostream& out, const Elements& stream) {
+    bool first_elem = true;
+    for (const auto& elem : stream) {
+        if (first_elem) {
+            first_elem = false;
+            out << elem;
+        }
+        else out << ", " << elem;
+    }
+    return out;
+}
 
-#define ASSERT_HINT(expr, hint) AssertImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint)) /* реализовать самостоятельно */
+template <typename Element>//, typename Element_s>
+ostream& operator<<(ostream& out, const vector<Element>& container) {
+    out << "[";
+    Print(out, container);
+    out << "]";
+    return out;
+}
+
+template <typename Element_f, typename Element_s>
+ostream& operator<<(ostream& out, const map<Element_f, Element_s>& container){
+    out << "{";
+    Print(out, container);
+    out << "}";
+    return out;
+}
+
+template <typename Element>
+ostream& operator<<(ostream& out, const set<Element>& container) {
+    out << "{";
+    Print(out, container);
+    out << "}";
+    return out;
+}
+
+template <typename T, typename U>
+void AssertEqualImpl(const T& t, const U& u, const string& t_str, const string& u_str, const string& file,
+                     const string& func, unsigned line, const string& hint) {
+    if (t != u) {
+        cout << boolalpha;
+        cout << file << "("s << line << "): "s << func << ": "s;
+        cout << "ASSERT_EQUAL("s << t_str << ", "s << u_str << ") failed: "s;
+        cout << t << " != "s << u << "."s;
+        if (!hint.empty()) {
+            cout << " Hint: "s << hint;
+        }
+        cout << endl;
+        abort();
+    }
+}
+
+#define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, ""s)
+
+#define ASSERT_EQUAL_HINT(a, b, hint) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, (hint))
+
+void AssertImpl(bool value, const string& expr_str, const string& file, const string& func, unsigned line,
+                const string& hint) {
+    if (!value) {
+        cout << file << "("s << line << "): "s << func << ": "s;
+        cout << "ASSERT("s << expr_str << ") failed."s;
+        if (!hint.empty()) {
+            cout << " Hint: "s << hint;
+        }
+        cout << endl;
+        abort();
+    }
+}
+
+#define ASSERT(expr) AssertImpl(!!(expr), #expr, __FILE__, __FUNCTION__, __LINE__, ""s)
+
+#define ASSERT_HINT(expr, hint) AssertImpl(!!(expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint))
+
+vector<int> TakeEvens(const vector<int>& numbers) {
+    vector<int> evens;
+    for (int x : numbers) {
+        if (x % 2 == 0) {
+            evens.push_back(x);
+        }
+    }
+    return evens;
+}
+
+map<string, int> TakeAdults(const map<string, int>& people) {
+    map<string, int> adults;
+    for (const auto& [name, age] : people) {
+        if (age >= 18) {
+            adults[name] = age;
+        }
+    }
+    return adults;
+}
+
+bool IsPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    int i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) {
+            return false;
+        }
+        ++i;
+    }
+    return true;
+}
+
+set<int> TakePrimes(const set<int>& numbers) {
+    set<int> primes;
+    for (int number : numbers) {
+        if (IsPrime(number)) {
+            primes.insert(number);
+        }
+    }
+    return primes;
+}
 
 int main() {
-    string hello = "hello"s;
-    ASSERT(!hello.empty());
-    ASSERT_HINT(2 + 2 == 5,"This will fail"s);
+    {
+        const set<int> numbers = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        const set<int> expected_primes = {2, 3, 5, 7, 11, 113};
+        ASSERT_EQUAL(TakePrimes(numbers), expected_primes);
+    }
+
+    {
+        const map<string, int> people = {{"Ivan"s, 19}, {"Sergey"s, 16}, {"Alexey"s, 18}};
+        const map<string, int> expected_adults = {{"Alexey"s, 18}, {"Ivan"s, 19}};
+        ASSERT_EQUAL(TakeAdults(people), expected_adults);
+    }
+
+    {
+        const vector<int> numbers = {3, 2, 1, 0, 3, 6};
+        const vector<int> expected_evens = {2, 0, 6};
+        ASSERT_EQUAL(TakeEvens(numbers), expected_evens);
+    }
 }
