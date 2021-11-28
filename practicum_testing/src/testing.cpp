4e40a131176b0d70f00a7b2cd1a0377c1a54f72f
evgsam <evgsamarin@gmail.com>
2021-11-28 12:08:49 +0300

    Фрейморк-тесты синонимы

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,56 +1,36 @@
-#include <cstdlib>
-#include <iomanip>
+#include <cassert>
 #include <iostream>
 #include <map>
 #include <set>
+#include <sstream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-template <typename Elements1, typename Elements2>
-ostream& operator<<(ostream& out, const pair<Elements1, Elements2>& stream) {
-    out <<stream.first<< ": "s<<stream.second;
-    return out;
+
+/*
+ Задание
+Замените встроенный макрос assert на макросы ASSERT_EQUAL, ASSERT_EQUAL_HINT,
+ASSERT и ASSERT_HINT из вашего фреймворка. Вам пригодится решение задания из урока
+«Юнит-тестирование класса». Используйте его в качестве заготовки кода.
+ */
+
+void AssertImpl(bool value, const string& t_str,  const string& file,
+                     const string& func, unsigned line, const string& hint) {
+   if (!value){
+       cout << boolalpha;
+       cout << file << "("s << line << "): "s << func << ": "s;
+       cout << "ASSERT("s << t_str << ") failed."s;
+       if (!hint.empty()) {
+           cout << " Hint: "s << hint;
+       }
+       cout << endl;
+       abort();
+
+   }
 }
 
-template <typename Elements>
-ostream& Print(ostream& out, const Elements& stream) {
-    bool first_elem = true;
-    for (const auto& elem : stream) {
-        if (first_elem) {
-            first_elem = false;
-            out << elem;
-        }
-        else out << ", " << elem;
-    }
-    return out;
-}
-
-template <typename Element>//, typename Element_s>
-ostream& operator<<(ostream& out, const vector<Element>& container) {
-    out << "[";
-    Print(out, container);
-    out << "]";
-    return out;
-}
-
-template <typename Element_f, typename Element_s>
-ostream& operator<<(ostream& out, const map<Element_f, Element_s>& container){
-    out << "{";
-    Print(out, container);
-    out << "}";
-    return out;
-}
-
-template <typename Element>
-ostream& operator<<(ostream& out, const set<Element>& container) {
-    out << "{";
-    Print(out, container);
-    out << "}";
-    return out;
-}
-
+//practicum_testing: ../src/testing.cpp:85: void TestAddingSynonymsIncreasesTheirCount(): Assertion `synonyms.GetSynonymCount("music"s) == 1' failed.
 template <typename T, typename U>
 void AssertEqualImpl(const T& t, const U& u, const string& t_str, const string& u_str, const string& file,
                      const string& func, unsigned line, const string& hint) {
@@ -67,87 +47,117 @@ void AssertEqualImpl(const T& t, const U& u, const string& t_str, const string& 
     }
 }
 
+
 #define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, ""s)
 
-#define ASSERT_EQUAL_HINT(a, b, hint) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, (hint))
+#define ASSERT(expr) AssertImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, ""s)/* реализовать самостоятельно */
 
-void AssertImpl(bool value, const string& expr_str, const string& file, const string& func, unsigned line,
-                const string& hint) {
-    if (!value) {
-        cout << file << "("s << line << "): "s << func << ": "s;
-        cout << "ASSERT("s << expr_str << ") failed."s;
-        if (!hint.empty()) {
-            cout << " Hint: "s << hint;
-        }
-        cout << endl;
-        abort();
+#define ASSERT_HINT(expr, hint) AssertImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint)) /* реализовать самостоятельно */
+
+
+class Synonyms {
+public:
+    void Add(const string& first_word, const string& second_word) {
+        synonyms_[first_word].insert(second_word);
+        synonyms_[second_word].insert(first_word);
     }
-}
 
-#define ASSERT(expr) AssertImpl(!!(expr), #expr, __FILE__, __FUNCTION__, __LINE__, ""s)
-
-#define ASSERT_HINT(expr, hint) AssertImpl(!!(expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint))
-
-vector<int> TakeEvens(const vector<int>& numbers) {
-    vector<int> evens;
-    for (int x : numbers) {
-        if (x % 2 == 0) {
-            evens.push_back(x);
+    size_t GetSynonymCount(const string& word) const {
+        if (synonyms_.count(word) != 0) {
+            return synonyms_.at(word).size();
         }
+        return 0;
     }
-    return evens;
-}
 
-map<string, int> TakeAdults(const map<string, int>& people) {
-    map<string, int> adults;
-    for (const auto& [name, age] : people) {
-        if (age >= 18) {
-            adults[name] = age;
+    bool AreSynonyms(const string& first_word, const string& second_word) const {
+        if (synonyms_.count(first_word) != 0) {
+            return synonyms_.at(first_word).count(second_word) != 0;
         }
-    }
-    return adults;
-}
-
-bool IsPrime(int n) {
-    if (n < 2) {
         return false;
     }
-    int i = 2;
-    while (i * i <= n) {
-        if (n % i == 0) {
-            return false;
-        }
-        ++i;
-    }
-    return true;
+
+private:
+    map<string, set<string>> synonyms_;
+};
+
+void TestAddingSynonymsIncreasesTheirCount() {
+    Synonyms synonyms;
+   // assert(synonyms.GetSynonymCount("music"s) == 0);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 0);
+
+   // assert(synonyms.GetSynonymCount("melody"s) == 0);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s),0);
+
+    synonyms.Add("music"s, "melody"s);
+
+    //assert(synonyms.GetSynonymCount("music"s) == 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 1);
+    //assert(synonyms.GetSynonymCount("melody"s) == 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s),1);
+
+    synonyms.Add("music"s, "tune"s);
+    //assert(synonyms.GetSynonymCount("music"s) == 2);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 2);
+
+    //assert(synonyms.GetSynonymCount("tune"s) == 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("tune"s), 1);
+
+   // assert(synonyms.GetSynonymCount("melody"s) == 1);
+    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s),1);
 }
 
-set<int> TakePrimes(const set<int>& numbers) {
-    set<int> primes;
-    for (int number : numbers) {
-        if (IsPrime(number)) {
-            primes.insert(number);
-        }
-    }
-    return primes;
+void TestAreSynonyms() {
+    Synonyms synonyms;
+    synonyms.Add("winner"s, "champion"s);
+    synonyms.Add("good"s, "nice"s);
+
+    //assert(synonyms.AreSynonyms("winner"s, "champion"s));
+    ASSERT(synonyms.AreSynonyms("winner"s, "champion"s));
+
+    //assert(synonyms.AreSynonyms("champion"s, "winner"s));
+    ASSERT(synonyms.AreSynonyms("champion"s, "winner"s));
+
+    //assert(!synonyms.AreSynonyms("good"s, "champion"s));
+    ASSERT(!synonyms.AreSynonyms("good"s, "champion"s));
+    
+    //assert(synonyms.AreSynonyms("good"s, "nice"s));
+    ASSERT(synonyms.AreSynonyms("good"s, "nice"s));
+}
+
+void TestSynonyms() {
+    TestAddingSynonymsIncreasesTheirCount();
+    TestAreSynonyms();
 }
 
 int main() {
-    {
-        const set<int> numbers = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        const set<int> expected_primes = {2, 3, 5, 7, 11, 113};
-        ASSERT_EQUAL(TakePrimes(numbers), expected_primes);
-    }
+    TestSynonyms();
 
-    {
-        const map<string, int> people = {{"Ivan"s, 19}, {"Sergey"s, 16}, {"Alexey"s, 18}};
-        const map<string, int> expected_adults = {{"Alexey"s, 18}, {"Ivan"s, 19}};
-        ASSERT_EQUAL(TakeAdults(people), expected_adults);
-    }
+    Synonyms synonyms;
 
-    {
-        const vector<int> numbers = {3, 2, 1, 0, 3, 6};
-        const vector<int> expected_evens = {2, 0, 6};
-        ASSERT_EQUAL(TakeEvens(numbers), expected_evens);
+    string line;
+    while (getline(cin, line)) {
+        istringstream command(line);
+        string action;
+        command >> action;
+
+        if (action == "ADD"s) {
+            string first_word, second_word;
+            command >> first_word >> second_word;
+            synonyms.Add(first_word, second_word);
+        } else if (action == "COUNT"s) {
+            string word;
+            command >> word;
+            cout << synonyms.GetSynonymCount(word) << endl;
+        } else if (action == "CHECK"s) {
+            string first_word, second_word;
+            command >> first_word >> second_word;
+            if (synonyms.AreSynonyms(first_word, second_word)) {
+                cout << "YES"s << endl;
+            } else {
+                cout << "NO"s << endl;
+            }
+        } else if (action == "EXIT") {
+            break;
+        }
     }
 }
