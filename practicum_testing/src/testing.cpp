7b4c7b3a1bf37ec66d45cd5c5a1a192a57f30093
user <user@samarin.ppsrk.ru>
2021-12-06 13:23:15 +0300

    юнит тестирование

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,163 +1,229 @@
-#include <cassert>
+//#include "search_server.h"
+
+#include <algorithm>
 #include <iostream>
 #include <map>
 #include <set>
-#include <sstream>
 #include <string>
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <cassert>
 
 using namespace std;
 
+/* Подставьте вашу реализацию класса SearchServer сюда */
 
 /*
- Задание
-Замените встроенный макрос assert на макросы ASSERT_EQUAL, ASSERT_EQUAL_HINT,
-ASSERT и ASSERT_HINT из вашего фреймворка. Вам пригодится решение задания из урока
-«Юнит-тестирование класса». Используйте его в качестве заготовки кода.
+ Подставьте сюда вашу реализацию макросов
+ ASSERT, ASSERT_EQUAL, ASSERT_EQUAL_HINT, ASSERT_HINT и RUN_TEST
  */
-
-void AssertImpl(bool value, const string& t_str,  const string& file,
-                     const string& func, unsigned line, const string& hint) {
-   if (!value){
-       cout << boolalpha;
-       cout << file << "("s << line << "): "s << func << ": "s;
-       cout << "ASSERT("s << t_str << ") failed."s;
-       if (!hint.empty()) {
-           cout << " Hint: "s << hint;
-       }
-       cout << endl;
-       abort();
-
-   }
+void Print(ostream &out, const DocumentStatus &s) {
+	if (s == DocumentStatus::ACTUAL) {
+		out << "DocumentStatus::ACTUAL"s;
+	} else if (s == DocumentStatus::IRRELEVANT) {
+		out << "DocumentStatus::IRRELEVANT"s;
+	} else if (s == DocumentStatus::BANNED) {
+		out << "DocumentStatus::BANNED"s;
+	} else if (s == DocumentStatus::REMOVED) {
+		out << "DocumentStatus::REMOVED"s;
+	}
+}
+ostream& operator<<(ostream &out, const DocumentStatus &v) {
+	Print(out, v);
+	return out;
 }
 
-//practicum_testing: ../src/testing.cpp:85: void TestAddingSynonymsIncreasesTheirCount(): Assertion `synonyms.GetSynonymCount("music"s) == 1' failed.
-template <typename T, typename U>
-void AssertEqualImpl(const T& t, const U& u, const string& t_str, const string& u_str, const string& file,
-                     const string& func, unsigned line, const string& hint) {
-    if (t != u) {
-        cout << boolalpha;
-        cout << file << "("s << line << "): "s << func << ": "s;
-        cout << "ASSERT_EQUAL("s << t_str << ", "s << u_str << ") failed: "s;
-        cout << t << " != "s << u << "."s;
-        if (!hint.empty()) {
-            cout << " Hint: "s << hint;
-        }
-        cout << endl;
-        abort();
-    }
+template
+void RunTestImpl(Func func, const string func_str) {
+	func();
+	cerr << func_str << " OK" << endl;
 }
 
+//#define RUN_TEST(func) RunTestImpl(func, #func)
 
-#define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, ""s)
-
-#define ASSERT(expr) AssertImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, ""s)/* реализовать самостоятельно */
-
-#define ASSERT_HINT(expr, hint) AssertImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, (hint)) /* реализовать самостоятельно */
-
-
-class Synonyms {
-public:
-    void Add(const string& first_word, const string& second_word) {
-        synonyms_[first_word].insert(second_word);
-        synonyms_[second_word].insert(first_word);
-    }
-
-    size_t GetSynonymCount(const string& word) const {
-        if (synonyms_.count(word) != 0) {
-            return synonyms_.at(word).size();
-        }
-        return 0;
-    }
-
-    bool AreSynonyms(const string& first_word, const string& second_word) const {
-        if (synonyms_.count(first_word) != 0) {
-            return synonyms_.at(first_word).count(second_word) != 0;
-        }
-        return false;
-    }
-
-private:
-    map<string, set<string>> synonyms_;
-};
-
-void TestAddingSynonymsIncreasesTheirCount() {
-    Synonyms synonyms;
-   // assert(synonyms.GetSynonymCount("music"s) == 0);
-    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 0);
-
-   // assert(synonyms.GetSynonymCount("melody"s) == 0);
-    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s),0);
-
-    synonyms.Add("music"s, "melody"s);
-
-    //assert(synonyms.GetSynonymCount("music"s) == 1);
-    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 1);
-    //assert(synonyms.GetSynonymCount("melody"s) == 1);
-    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s),1);
-
-    synonyms.Add("music"s, "tune"s);
-    //assert(synonyms.GetSynonymCount("music"s) == 2);
-    ASSERT_EQUAL(synonyms.GetSynonymCount("music"s), 2);
-
-    //assert(synonyms.GetSynonymCount("tune"s) == 1);
-    ASSERT_EQUAL(synonyms.GetSynonymCount("tune"s), 1);
-
-   // assert(synonyms.GetSynonymCount("melody"s) == 1);
-    ASSERT_EQUAL(synonyms.GetSynonymCount("melody"s),1);
+template<typename T, typename U>
+void AssertEqualImpl(const T &t, const U &u, const string &t_str,
+		const string &u_str, const string &file, const string &func,
+		unsigned line, const string &hint) {
+	if (t != u) {
+		cerr << boolalpha;
+		cerr << file << "("s << line << "): "s << func << ": "s;
+		cerr << "ASSERT_EQUAL("s << t_str << ", "s << u_str << ") failed: "s;
+		cerr << t << " != "s << u << "."s;
+		if (!hint.empty()) {
+			cerr << " Hint: "s << hint;
+		}
+		cerr << endl;
+		abort();
+	}
 }
 
-void TestAreSynonyms() {
-    Synonyms synonyms;
-    synonyms.Add("winner"s, "champion"s);
-    synonyms.Add("good"s, "nice"s);
+void AssertImpl(bool value, const string &expr_str, const string &file,
+		const string &func, unsigned line, const string &hint) {
+	if (!value) {
+		cerr << file << "("s << line << "): "s << func << ": "s;
+		cerr << "Assert("s << expr_str << ") failed."s;
+		if (!hint.empty()) {
+			cerr << " Hint: "s << hint;
+		}
+		cerr << endl;
+		abort();
+	}
+}
+//#define ASSERT(expr) AssertImpl(!!(expr), #expr, FILE, FUNCTION, LINE, ""s)
 
-    //assert(synonyms.AreSynonyms("winner"s, "champion"s));
-    ASSERT(synonyms.AreSynonyms("winner"s, "champion"s));
+//#define ASSERT_HINT(expr, hint) AssertImpl(!!(expr), #expr, FILE, FUNCTION, LINE, (hint))
 
-    //assert(synonyms.AreSynonyms("champion"s, "winner"s));
-    ASSERT(synonyms.AreSynonyms("champion"s, "winner"s));
+//#define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, FILE, FUNCTION, LINE, ""s)
 
-    //assert(!synonyms.AreSynonyms("good"s, "champion"s));
-    ASSERT(!synonyms.AreSynonyms("good"s, "champion"s));
-    
-    //assert(synonyms.AreSynonyms("good"s, "nice"s));
-    ASSERT(synonyms.AreSynonyms("good"s, "nice"s));
+//#define ASSERT_EQUAL_HINT(a, b, hint) AssertEqualImpl((a), (b), #a, #b, FILE, FUNCTION, LINE, (hint))
+
+// -------- Начало модульных тестов поисковой системы ----------
+
+// Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов
+void TestExcludeStopWordsFromAddedDocumentContent() {
+	const int doc_id = 42;
+	const string content = "cat in the city"s;
+	const vector ratings = { 1, 2, 3 };
+	{
+		SearchServer server;
+		server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+		const auto found_docs = server.FindTopDocuments("in"s);
+		ASSERT_EQUAL(found_docs.size(), 1u);
+		const Document &doc0 = found_docs[0];
+		ASSERT_EQUAL(doc0.id, doc_id);
+	}
+
+	{
+
+		SearchServer server;
+
+		server.SetStopWords("in the"s);
+
+		server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+
+		ASSERT_HINT(server.FindTopDocuments("in"s).empty(),
+				"Stop words must be excluded from documents"s);
+
+	}
+
 }
 
-void TestSynonyms() {
-    TestAddingSynonymsIncreasesTheirCount();
-    TestAreSynonyms();
+//Тест проверяет, что стоп-слова исключаются из текста документов
+void TestStopWords() {
+	SearchServer server;
+	server.AddDocument(1, "cat in the city", DocumentStatus::ACTUAL,
+			{ 1, 2, 3 });
+	auto found_doc = server.MatchDocument("cat in the city"s, 1);
+	vector words = get < 0 > (found_doc);
+	ASSERT_EQUAL(words[0], "cat"s);
+	ASSERT_EQUAL(words[1], "city"s);
+	ASSERT_EQUAL(words[2], "in"s);
+	ASSERT_EQUAL(words[3], "the"s);
+	server.SetStopWords("in the"s); //добавляем стоп- слова
+	found_doc = server.MatchDocument("cat in the city"s, 1);
+	words = get < 0 > (found_doc);
+	ASSERT_EQUAL(words.size(), 2u);
+	ASSERT_EQUAL(words[0], "cat"s);
+	ASSERT_EQUAL(words[1], "city"s);
 }
+//Тест проверяет, что документы, содержащие минус-слова из поискового запроса, не включаются в результаты поиска.
+void TestMinusWords() {
+	SearchServer server;
+	server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL,
+			{ 8, -3 });
+	server.AddDocument(1, "пушистый кот пушистый хвост"s,
+			DocumentStatus::ACTUAL, { 7, 2, 7 });
+	server.AddDocument(2, "ухоженный пёс выразительные глаза"s,
+			DocumentStatus::ACTUAL, { 5, -12, 2, 1 });
+	auto found_docs = server.FindTopDocuments("ухоженный кот"s);
+	ASSERT_EQUAL(found_docs.size(), 3u);
+	found_docs = server.FindTopDocuments("ухоженный -кот"s);
+	ASSERT_EQUAL_HINT(found_docs.size(), 1u,
+			"Documents containing minus words from the search query are not included in the search"s);
+}
+void TestRating() {
+	SearchServer server;
+	server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL,
+			{ 7, 2, 6 });
+	auto found_docs = server.FindTopDocuments("пушистый кот"s);
+	ASSERT_EQUAL(found_docs[0].rating, 5);
+}
+void TestCountRelevance() {
+	SearchServer server;
+	server.AddDocument(0, "белый кот модный ошейник"s, DocumentStatus::ACTUAL, {
+			8, -3 });
+	server.AddDocument(1, "пушистый хвост"s, DocumentStatus::ACTUAL,
+			{ 7, 2, 7 });
+	auto found_docs = server.FindTopDocuments("пушистый кот"s);
+	ASSERT(abs(found_docs[0].relevance - 0.346574) < 1e-6);
+	ASSERT(abs(found_docs[1].relevance - 0.1732874) < 1e-6);
+}
+//Тест на соответствие документов поисковому запросу. Должны быть возвращены все слова из поискового запроса, присутствующие в документе. Если есть соответствие хотя бы по одному минус-слову, возвращается пустой список слов
+void TestMatchDocument() {
+	const int doc_id = 42;
+	const string content = "cat in the city"s;
+	const vector ratings = { 1, 2, 3 };
+	SearchServer server;
+	server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+	const string query = "cat city";
+	auto mathdo = server.MatchDocument(query, doc_id);
+	vector words = get < 0 > (mathdo);
+	DocumentStatus s = get < 1 > (mathdo);
+	ASSERT_EQUAL(words[0], "cat");
+	ASSERT_EQUAL(words[1], "city");
+	ASSERT_EQUAL(s, DocumentStatus::ACTUAL);
+	mathdo = server.MatchDocument("cat -in"s, doc_id);
+	words = get < 0 > (mathdo);
+	ASSERT(words.empty());
+}
+void TestAddDocument() {
+	SearchServer server;
+	const int doc_id = 4;
+	const string text = "cat and dog in the city"s;
+	const vector ratings = { 1, 2, 3 };
+	ASSERT(server.FindTopDocuments("dog"s).empty());
+	server.AddDocument(doc_id, text, DocumentStatus::ACTUAL, ratings);
+	const auto found_docs = server.FindTopDocuments("dog in the city"s);
+	ASSERT_EQUAL(found_docs.size(), 1u);
+}
+void TestFindTopDocuments() {
+	SearchServer server;
+	ASSERT(server.FindTopDocuments("cat"s).empty());
+	server.AddDocument(1, "cat and dog in the city", DocumentStatus::ACTUAL, {
+			1, 3, 5 });
+	server.AddDocument(2, "small cat and big dogs"s, DocumentStatus::BANNED, {
+			4, 2, 1 });
+	server.AddDocument(0, "white cat on the street"s, DocumentStatus::ACTUAL, {
+			1, 3, 5 });
+	auto found_docs = server.FindTopDocuments("cat"s);
+	ASSERT_EQUAL(found_docs.size(), 2u);
+	DocumentStatus status = DocumentStatus::BANNED;
+	found_docs = server.FindTopDocuments("cat"s, status);
+	ASSERT_EQUAL(found_docs.size(), 1u);
+	found_docs = server.FindTopDocuments("cat"s,
+			[](int document_id, DocumentStatus status, int rating) {
+				return document_id % 2 == 0;
+			});
+	ASSERT_EQUAL(found_docs.size(), 2u);
+
+}
+// Функция TestSearchServer является точкой входа для запуска тестов
+void TestSearchServer() {
+	RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
+	RUN_TEST(TestStopWords);
+	RUN_TEST(TestMinusWords);
+	RUN_TEST(TestRating);
+	RUN_TEST(TestCountRelevance);
+	RUN_TEST(TestMatchDocument);
+	RUN_TEST(TestAddDocument);
+}
+
+// --------- Окончание модульных тестов поисковой системы -----------
 
 int main() {
-    TestSynonyms();
-
-    Synonyms synonyms;
-
-    string line;
-    while (getline(cin, line)) {
-        istringstream command(line);
-        string action;
-        command >> action;
-
-        if (action == "ADD"s) {
-            string first_word, second_word;
-            command >> first_word >> second_word;
-            synonyms.Add(first_word, second_word);
-        } else if (action == "COUNT"s) {
-            string word;
-            command >> word;
-            cout << synonyms.GetSynonymCount(word) << endl;
-        } else if (action == "CHECK"s) {
-            string first_word, second_word;
-            command >> first_word >> second_word;
-            if (synonyms.AreSynonyms(first_word, second_word)) {
-                cout << "YES"s << endl;
-            } else {
-                cout << "NO"s << endl;
-            }
-        } else if (action == "EXIT") {
-            break;
-        }
-    }
+	TestSearchServer();
+// Если вы видите эту строку, значит все тесты прошли успешно
+	cout << "Search server testing finished"s << endl;
 }
