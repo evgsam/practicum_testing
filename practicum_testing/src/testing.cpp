14a987cf6ccc2feb62af47987edd2696c03487f0
user <user@samarin.ppsrk.ru>
2021-12-22 17:52:47 +0300

    итератор в методах контейнера

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,8 +1,8 @@
 #include <algorithm>
 #include <iostream>
-#include <set>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -14,23 +14,18 @@ void PrintRange(It range_begin, It range_end) {
     cout << endl;
 }
 
-template <typename It>
-auto MakeSet(It range_begin, It range_end) {
-    return set(range_begin, range_end);
-}
 
-template <typename It>
-auto MakeVector(It range_begin, It range_end) {
-    return vector(range_begin, range_end);
+template <typename Container, typename Iterator>
+void EraseAndPrint(Container& container, Iterator iterator) {
+	PrintRange(container.begin(), iterator);
+	auto iter = find(container.begin(), container.end(), *iterator);
+    auto it_begin=container.erase(iter);
+	auto it_end=end(container);
+	PrintRange(it_begin, it_end);
 }
 
 
 int main() {
-    //vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s, "C++"s, "C++"s, "Ruby"s, "Java"s};
-	set<string> unique_langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
-    vector<string> langs = MakeVector(unique_langs.begin(), unique_langs.end());
-    PrintRange(langs.begin(), langs.end());
-    /*  auto unique_langs = MakeSet(langs.begin(), langs.end());
-    PrintRange(unique_langs.begin(), unique_langs.end());
-    */
+    set<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
+    EraseAndPrint(langs, langs.begin());
 }
