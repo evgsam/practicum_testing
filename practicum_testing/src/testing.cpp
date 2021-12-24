720c5929f2fab64db783aed3d95c0f731754b67f
user <user@samarin.ppsrk.ru>
2021-12-24 17:33:44 +0300

    Ханойская башня. Наброски из практикума

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,35 +1,76 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
+#include <stdexcept>
 #include <vector>
-#include <set>
 
 using namespace std;
 
-uint64_t Factorial(int num) {
-	int factorial = 1;
-	if (0 != num) {
-		factorial = Factorial(num - 2) * num;
-	}
-	return factorial;
+class Tower {
+public:
+    // конструктор и метод SetDisks нужны, чтобы правильно создать башни
+    Tower(int disks_num) {
+        FillTower(disks_num);
+    }
+
+    int GetDisksNum() const {
+        return disks_.size();
+    }
+
+    void SetDisks(int disks_num) {
+        FillTower(disks_num);
+    }
+
+    // добавляем диск на верх собственной башни
+    // обратите внимание на исключение, которое выбрасывается этим методом
+    void AddToTop(int disk) {
+        int top_disk_num = disks_.size() - 1;
+        if (0 != disks_.size() && disk >= disks_[top_disk_num]) {
+            throw invalid_argument("Невозможно поместить большой диск на маленький");
+        } else {
+            // допишите этот метод и используйте его в вашем решении
+        }
+    }
+
+    // вы можете дописывать необходимые для вашего решения методы
+
+private:
+    vector<int> disks_;
+
+    // используем приватный метод FillTower, чтобы избежать дубликации кода
+    void FillTower(int disks_num) {
+        for (int i = disks_num; i > 0; i--) {
+            disks_.push_back(i);
+        }
+    }
+};
+
+
+// disks_num - количество перемещаемых дисков
+// destination - конечная башня для перемещения
+// buffer - башня, которую нужно использовать в качестве буфера для дисков
+void MoveDisks(int disks_num, Tower& destination, Tower& buffer) {
+    if (// условие выхода ещё не выполнено) {
+        // действия из шага рекурсии
+    }
 }
 
-
-bool IsPowOfTwo(int num) {
-	if (num == 0) {
-		return false;
-	}
-	if (num == 1) {
-		return true;
-	}
-	if (num % 2 == 0) {
-		return IsPowOfTwo(num/2);
-	}
-	return false;
+void SolveHanoi(vector<Tower>& towers) {
+    int disks_num = towers[0].GetDisksNum();
+    // запускаем рекурсию
+    // просим переложить все диски на последнюю башню
+    // с использованием средней башни как буфера
+    towers[0].MoveDisks(disks_num, towers[2], towers[1]);
 }
-
 
 int main() {
-    int result = IsPowOfTwo(3);
-    cout << result << endl;
+    int towers_num = 3;
+    int disks_num = 3;
+    vector<Tower> towers;
+    // добавим в вектор три пустые башни
+    for (int i = 0; i < towers_num; ++i) {
+        towers.push_back(0);
+    }
+    // добавим на первую башню три кольца
+    towers[0].SetDisks(disks_num);
+
+    SolveHanoi(towers);
 }
