df8531714cd966b27ef763da1d450ee7ac9369b7
user <user@samarin.ppsrk.ru>
2021-12-24 13:42:59 +0300

    рекурсия фибоначи

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -6,25 +6,24 @@
 
 using namespace std;
 
-template <typename It>
-void PrintRange(It range_begin, It range_end) {
-    for (auto it = range_begin; it != range_end; ++it) {
-        cout << *it << " "s;
-    }
-    cout << endl;
+uint64_t Factorial(int num) {
+	int factorial = 1;
+	if (0 != num) {
+		factorial = Factorial(num - 2) * num;
+	}
+	return factorial;
 }
 
-
-//template <typename Container, typename Iterator>
-template <typename Container>
-void EraseAndPrint(Container& container, int pos, int begin,int end) {
-	auto it_to_erased = container.erase(container.begin()+pos);
-    PrintRange(container.begin(), container.end());
-    it_to_erased = container.erase(container.begin()+begin,container.begin()+end);
-    PrintRange(container.begin(), container.end());
+uint64_t Fibonacci(int num) {
+	if (num == 0) {
+		return 0;
+	}
+	if (num == 1) {
+		return 1;
+	}
+	return Fibonacci(num - 1) + Fibonacci(num - 2);
 }
 
 int main() {
-    vector <string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
-    EraseAndPrint(langs, 0,0,2);
+	cout << Fibonacci(6) << endl;
 }
