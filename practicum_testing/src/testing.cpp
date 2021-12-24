0ba45ebdd3c252204f1e8a34f42a357669de8da5
evgsam <evgsamarin@gmail.com>
2021-12-24 21:14:11 +0300

    ХБ:печать состояния

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -18,30 +18,62 @@ public:
 		FillTower(disks_num);
 	}
 
-    // добавляем диск на верх собственной башни
-    // обратите внимание на исключение, которое выбрасывается этим методом
-    void AddToTop(int disk) {
-        int top_disk_num = disks_.size() - 1;
-        if (0 != disks_.size() && disk >= disks_[top_disk_num]) {
-            throw invalid_argument("Невозможно поместить большой диск на маленький");
-        } else {
-           disks_.push_back(disk);
-        }
-    }
+	// добавляем диск на верх собственной башни
+	// обратите внимание на исключение, которое выбрасывается этим методом
+	void AddToTop(int disk) {
+		int top_disk_num = disks_.size() - 1;
+		if (0 != disks_.size() && disk >= disks_[top_disk_num]) {
+			throw invalid_argument(
+					"Невозможно поместить большой диск на маленький");
+		} else {
+			disks_.push_back(disk);
+		}
+	}
+	void PrintDisks(int disk_num,Tower &from_tower, Tower &destination, Tower &buffer){
+		cout<<"Disk number:"<<disk_num<<endl;
+		for(auto vec:from_tower.disks_){
+			cout<<vec<<" ";
+		}
+		cout<<endl;
+		if (buffer.disks_.empty()){
+			for (int i=0;i<from_tower.disks_.size();i++){
+				cout<<"0 ";
+			}
+		}
+		else{
+			for(auto vec:buffer.disks_){
+				cout<<vec;
+			}
+		}
+		cout<<endl;
+		if (destination.disks_.empty()){
+			for (int i=0;i<from_tower.disks_.size();i++){
+				cout<<"0 ";
+			}
+		}
+		else{
+			for(auto vec:destination.disks_){
+				cout<<vec;
+			}
+		}
+		cout<<endl;
+	}
 
-    // disks_num - количество перемещаемых дисков
-    // destination - конечная башня для перемещения
-    // buffer - башня, которую нужно использовать в качестве буфера для дисков
-    void MoveDisks(int disks_num, Tower& destination, Tower& buffer) {
-        if (disks_num > 1) {
-              Tower from_tower = *this;
-              from_tower.MoveDisks(disks_num-1, buffer, destination);
-             // buffer.MoveDisks(disks_num-1, destination, from_tower);
-          } else {
-              destination.AddToTop(disks_[disks_.size()-1]);
-          }
-    }
-    // вы можете дописывать необходимые для вашего решения методы
+	// disks_num - количество перемещаемых дисков
+	// destination - конечная башня для перемещения
+	// buffer - башня, которую нужно использовать в качестве буфера для дисков
+	void MoveDisks(int disks_num, Tower &destination, Tower &buffer) {
+		if (disks_num > 1) {
+			Tower from_tower = *this;
+			PrintDisks(disks_num, from_tower, destination, buffer);
+			from_tower.MoveDisks(disks_num - 1, buffer, destination);
+			from_tower.disks_.pop_back();
+			//buffer.MoveDisks(disks_num-1, destination, from_tower);
+		} /*else {
+			destination.AddToTop(disks_[disks_.size() - 1]);
+		}*/
+	}
+	// вы можете дописывать необходимые для вашего решения методы
 
 private:
 	vector<int> disks_;
