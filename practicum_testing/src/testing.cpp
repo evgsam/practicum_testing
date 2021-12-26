8a8bbc92bccaf042579eccd3810d1d09bf430506
evgsam <evgsamarin@gmail.com>
2021-12-26 19:48:53 +0300

    реализация "стека"

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,38 +1,54 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
-#include <utility>
 #include <vector>
 
 using namespace std;
 
-template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, string prefix) {
-    // Все строки, начинающиеся с prefix, больше или равны строке "<prefix>"
-    auto left = lower_bound(range_begin, range_end, prefix);
-    // Составим строку, которая в рамках буквенных строк является точной верхней гранью
-    // множества строк, начинающихся с prefix
-    string upper_bound = prefix;
-    ++upper_bound[upper_bound.size() - 1];
-    // Первое встреченное слово, не меньшее upper_bound, обязательно является концом полуинтервала
-    auto right = lower_bound(range_begin, range_end, upper_bound);
-    return {left, right};
+template<typename It>
+void PrintRange(It range_begin, It range_end) {
+	for (auto it = range_begin; it != range_end; ++it) {
+		cout << *it << " "s;
+	}
+	cout << endl;
 }
 
+template<typename Type>
+class Stack {
+public:
+	void Push(const Type &element) {
+
+		// напишите реализацию
+	}
+	void Pop() {
+		// напишите реализацию
+	}
+	const Type& Peek() const {
+		// напишите реализацию
+	}
+	Type& Peek() {
+		// напишите реализацию
+	}
+	void Print() const {
+		// напишите реализацию
+	}
+	uint64_t Size() const {
+		// напишите реализацию
+	}
+	bool IsEmpty() const {
+		// напишите реализацию
+	}
+
+private:
+	vector<Type> elements_;
+};
+
 int main() {
-    const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
-
-    const auto mo_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
-    for (auto it = mo_result.first; it != mo_result.second; ++it) {
-        cout << *it << " ";
+    Stack<int> stack;
+    for (uint32_t i = 0; i < 10; ++i) {
+        stack.Push(i);
+        stack.Print();
     }
-    cout << endl;
-
-    const auto mt_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
-    cout << (mt_result.first - begin(sorted_strings)) << " " << (mt_result.second - begin(sorted_strings)) << endl;
-
-    const auto na_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
-    cout << (na_result.first - begin(sorted_strings)) << " " << (na_result.second - begin(sorted_strings)) << endl;
-
-    return 0;
+    while (!stack.IsEmpty()) {
+        stack.Pop();
+        stack.Print();
+    }
 }
