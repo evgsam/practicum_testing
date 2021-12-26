290e179793bf726a939c4173935c8ffcc5eb2f97
evgsam <evgsamarin@gmail.com>
2021-12-26 18:42:31 +0300

    как предыдущая, но со строкой

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -8,37 +8,35 @@
 using namespace std;
 
 template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, char prefix) {
+pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, string prefix) {
     // Все строки, начинающиеся с '<prefix>', больше или равны строке "<prefix>"
-    auto left = lower_bound(range_begin, range_end, string(1, prefix));
-    // Составим следующий в алфавите символ.
-    // Не страшно, если prefix = 'z':
-    //в этом случае мы получим следующий за 'z' символ в таблице символов
-    char next_prefix = static_cast<char>(prefix + 1);
+	auto left = lower_bound(range_begin, range_end, prefix);
+	//ищем ближайшую строку - в которой последняя буква на 1 больше в алфавите
+	string next_prefix =string(prefix.begin(),prefix.end()-1)+ static_cast<char>(prefix[prefix.size()-1]+1);
 
     // Строка "<next_prefix>" в рамках буквенных строк
     // является точной верхней гранью
     // множества строк, начнающихся с '<prefix>'
-    auto right = lower_bound(range_begin, range_end, string(1, next_prefix));
+    auto right = lower_bound(range_begin, range_end, next_prefix);
 
     return {left, right};
 }
 
-
 int main() {
-	const vector<string> sorted_strings = { "moscow", "murmansk", "vologda" };
-	const auto m_result = FindStartsWith(begin(sorted_strings),
-			end(sorted_strings), 'm');
-	for (auto it = m_result.first; it != m_result.second; ++it) {
-	 cout << *it << " ";
-	 }
-	 cout << endl;
+    const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
 
-	 const auto p_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), 'p');
-	 cout << (p_result.first - begin(sorted_strings)) << " " << (p_result.second - begin(sorted_strings)) << endl;
+    const auto mo_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+    for (auto it = mo_result.first; it != mo_result.second; ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
 
-	 const auto z_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), 'z');
-	 cout << (z_result.first - begin(sorted_strings)) << " " << (z_result.second - begin(sorted_strings)) << endl;
+    const auto mt_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+    cout << (mt_result.first - begin(sorted_strings)) << " " << (mt_result.second - begin(sorted_strings)) << endl;
 
-	return 0;
+    const auto na_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+    cout << (na_result.first - begin(sorted_strings)) << " " << (na_result.second - begin(sorted_strings)) << endl;
+
+    return 0;
 }
+
