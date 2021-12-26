1e00a015660b3f0de4e0bf82c3980800ff9aff72
evgsam <evgsamarin@gmail.com>
2021-12-26 14:53:21 +0300

    поиск в отсортированном контейнере

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
 #include <iterator>
 #include <set>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-set<int>::const_iterator FindNearestElement(const set<int>& numbers, int border) {
-    // Запрашиваем итератор на первый элемент, не меньший border;
-    // если такого элемента нет, то мы получим numbers.end()
-    const auto first_not_less = numbers.lower_bound(border);
+template <typename RandomIt>
+pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, char prefix) {
+    // Все строки, начинающиеся с '<prefix>', больше или равны строке "<prefix>"
+    auto left = lower_bound(range_begin, range_end, string(1, prefix));
+    // Составим следующий в алфавите символ.
+    // Не страшно, если prefix = 'z':
+    //в этом случае мы получим следующий за 'z' символ в таблице символов
+    char next_prefix = static_cast<char>(prefix + 1);
 
-    // Если множество пусто или до первого элемента не меньше border
-    // нет элементов, то мы уже получили ответ
-    if (first_not_less == numbers.begin()) {
-        return first_not_less;
-    }
+    // Строка "<next_prefix>" в рамках буквенных строк
+    // является точной верхней гранью
+    // множества строк, начнающихся с '<prefix>'
+    auto right = lower_bound(range_begin, range_end, string(1, next_prefix));
 
-    // Если элементов, не меньших border, нет и set не пуст, то достаточно взять
-    // итератор на последний элемент, меньший border
-    // prev -> http://ru.cppreference.com/w/cpp/iterator/prev
-    const auto last_less = prev(first_not_less);
-    if (first_not_less == numbers.end()) {
-        return last_less;
-    }
-
-    // Разыменуем оба итератора-кандидата и выберем тот,
-    // чей элемент ближе к искомому
-    const bool is_left = (border - *last_less <= *first_not_less - border);
-    return is_left ? last_less : first_not_less;
+    return {left, right};
 }
 
+
 int main() {
-    set<int> numbers = {1, 4, 6};
-    cout << *FindNearestElement(numbers, 0) << " " << *FindNearestElement(numbers, 3) << " "
-         << *FindNearestElement(numbers, 5) << " " << *FindNearestElement(numbers, 6) << " "
-         << *FindNearestElement(numbers, 100) << endl;
+	const vector<string> sorted_strings = { "moscow", "murmansk", "vologda" };
+	const auto m_result = FindStartsWith(begin(sorted_strings),
+			end(sorted_strings), 'm');
+	for (auto it = m_result.first; it != m_result.second; ++it) {
+	 cout << *it << " ";
+	 }
+	 cout << endl;
 
-    set<int> empty_set;
+	 const auto p_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), 'p');
+	 cout << (p_result.first - begin(sorted_strings)) << " " << (p_result.second - begin(sorted_strings)) << endl;
 
-    cout << (FindNearestElement(empty_set, 8) == end(empty_set)) << endl;
-    return 0;
+	 const auto z_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), 'z');
+	 cout << (z_result.first - begin(sorted_strings)) << " " << (z_result.second - begin(sorted_strings)) << endl;
+
+	return 0;
 }
