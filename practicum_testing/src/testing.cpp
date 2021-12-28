74d7fb96d98bd467f37509959d2c08179530363e
user <user@samarin.ppsrk.ru>
2021-12-28 14:29:30 +0300

    "очередь"

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,4 +1,4 @@
-//авторское решение - https://onlinegdb.com/SyoYp2CPw
+#include <stack>
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -16,138 +16,92 @@ void PrintRange(It range_begin, It range_end) {
 }
 
 template<typename Type>
-class Stack {
+class Queue {
 public:
 	void Push(const Type &element) {
-		elements_.push_back(element);
+		stack1_.push(element);
 		// напишите реализацию
 	}
 	void Pop() {
-		elements_.pop_back();
+		stack2_.pop();
 		// напишите реализацию
 	}
-	const Type& Peek() const {
-		return elements_.back();
-	}
-	Type& Peek() {
-		return elements_.back();
-		// напишите реализацию
-	}
-	void Print() const {
-		PrintRange(elements_.begin(), elements_.end());
-	}
-	uint64_t Size() const {
-		return static_cast<int64_t>(elements_.size());
-	}
-	bool IsEmpty() const {
-		return elements_.empty();
-	}
 
-private:
-	vector<Type> elements_;
-};
-
-template<typename Type>
-class StackMin {
-public:
-	void Push(const Type &element) {
-		elements_.Push(element);
-		elements_minimum_.insert(
-				make_pair(element, static_cast<int>(Size() - 1)));
-	}
-	void Pop() {
-		pair<int, int> position;
-		elements_minimum_.erase(Peek());
-		elements_.Pop();
-	}
-	const Type& Peek() const {
-		return elements_.Peek();
-	}
-	Type& Peek() {
-		return elements_.Peek();
-	}
-	void Print() const {
-		elements_.Print();
-	}
-	uint64_t Size() const {
-		return elements_.Size();
-	}
-	bool IsEmpty() const {
-		return elements_.IsEmpty();
-	}
-	const Type& PeekMin() const {
-		return elements_minimum_.begin()->first;
-	}
-
-private:
-	Stack<Type> elements_;
-	map<Type, int> elements_minimum_;
-};
-
-template<typename Type>
-class SortedStack {
-public:
-	void Ssort(Type pointer, const Type &element) {
-		if ((element > pointer)&&(!IsEmpty())) {
-			elements_reserve_.push_back(Peek());
-			elements_.Pop();
-			pointer=Peek();
-			Ssort(pointer, element);
-			elements_.Push(elements_reserve_.back());
-			elements_reserve_.pop_back();
-		}
-		else {
-			elements_.Push(element);
+	void Reconstr(void) {
+		if (!stack2_.empty()) {
+			stack1_.push(stack2_.top());
+			stack2_.pop();
+			Reconstr();
 		}
 	}
-	void Push(const Type &element) {
+
+	Type& SearchFront(Type res) {
 		if (!IsEmpty()) {
-			Type& pointer=Peek();
-				Ssort(pointer,element);
+			stack2_.push(stack1_.top());
+			res = stack2_.top();
+			stack1_.pop();
+			SearchFront(res);
+			return res;
+		}
 
-			//Ssort(pointer,element);
+	}
+	Type& Front() {
+		Type res;
+		if (Size() == 1) {
+			return stack1_.top();
+		} else {
+			return SearchFront(res);
 		}
-		else {
-			elements_.Push(element);
-		}
+
+		// напишите реализацию
 	}
-	void Pop() {
-		elements_.Pop();
-		// напишите реализацию метода
+	const Type& Front() const {
+		return SearchFront();
+		// напишите реализацию
 	}
-	const Type& Peek() const {
-		return elements_.Peek();
-	}
-	Type& Peek() {
-		return elements_.Peek();
-	}
-	void Print() const {
-		elements_.Print();
-	}
+
 	uint64_t Size() const {
-		return elements_.Size();
+		return static_cast<int64_t>(stack1_.size());
+		// напишите реализацию
 	}
 	bool IsEmpty() const {
-		return elements_.IsEmpty();
+		return stack1_.empty();
+		// напишите реализацию
 	}
+
 private:
-	Stack<Type> elements_;
-	vector<Type> elements_reserve_;
+	stack<Type> stack1_;
+	stack<Type> stack2_;
 };
 
 int main() {
-	SortedStack<int> stack;
+	Queue<int> queue;
 	vector<int> values(5);
 
-	// заполняем вектор для тестирования нашего стека
+	// заполняем вектор для тестирования очереди
 	iota(values.begin(), values.end(), 1);
 	// перемешиваем значения
 	random_shuffle(values.begin(), values.end());
 
-	// заполняем стек и проверяем, что сортировка сохраняется после каждой вставки
+	PrintRange(values.begin(), values.end());
+
+	cout << "Заполняем очередь"s << endl;
+
+	// заполняем очередь и выводим элемент в начале очереди
 	for (int i = 0; i < 5; ++i) {
-		cout << "Вставляемый элемент = "s << values[i] << endl;
-		stack.Push(values[i]);
-		stack.Print();
+		queue.Push(values[i]);
+		cout << "Вставленный элемент "s << values[i] << endl;
+		cout << "Первый элемент очереди "s << queue.Front() << endl;
 	}
+
+	cout << "Вынимаем элементы из очереди"s << endl;
+
+	// выводим элемент в начале очереди и вытаскиваем элементы по одному
+	while (!queue.IsEmpty()) {
+		// сначала будем проверять начальный элемент, а потом вытаскивать,
+		// так как операция Front на пустой очереди не определена
+		cout << "Будем вынимать элемент "s << queue.Front() << endl;
+		queue.Pop();
+	}
+	return 0;
 }
