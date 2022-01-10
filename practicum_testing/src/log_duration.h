11d18e73698e40e9c076c8dac47e210fd1116020
user <user@samarin.ppsrk.ru>
2022-01-10 18:32:21 +0300

    упрощаем логгирование

---
+++ practicum_testing/src/log_duration.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+class LogDuration {
+public:
+    // заменим имя типа std::chrono::steady_clock
+    // с помощью using для удобства
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(std::string functrionName): FN(functrionName){
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        std::cerr << FN << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const Clock::time_point start_time_ = Clock::now();
+    std::string FN;
+};
--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,20 +1,17 @@
+#include <chrono>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-#include <chrono>
-#include <thread>
-
+#include "log_duration.h"
 
 using namespace std;
-using namespace chrono;
-// хотите немного магии? тогда используйте namespace literals
-using namespace literals;
 
 vector<int> ReverseVector(const vector<int> &source_vector) {
 	vector<int> res;
 	for (int i : source_vector) {
 		res.insert(res.begin(), i);
 	}
+
 	return res;
 }
 
@@ -29,50 +26,34 @@ int CountPops(const vector<int> &source_vector, int begin, int end) {
 }
 
 void FillRandom(vector<int> &v, int n) {
-
 	for (int i = 0; i < n; ++i) {
-		// получаем случайное число с помощью функции rand.
-		// с помощью (rand() % 2) получим целое число в диапазоне 0..1.
-		// в C++ имеются более современные генераторы случайных чисел,
-		// но в данном уроке не будем их касаться
 		v.push_back(rand() % 2);
 	}
 }
 
 void Operate() {
 	vector<int> random_bits;
-
-	// операция << для целых чисел это сдвиг всех бит в двоичной
-	// записи числа. Запишем с её помощью число 2 в степени 17 (131072)
+	LogDuration ld("Total"s);
 	static const int N = 1 << 17;
-
-	// заполним вектор случайными числами 0 и 1
-	const auto start_time = steady_clock::now();
-	FillRandom(random_bits, N);
-	const auto end_time = steady_clock::now();
-	const auto dur = end_time - start_time;
-	cerr<< "Fill random: "s << chrono::duration_cast<chrono::milliseconds>(dur).count() << " ms"s << endl;
-
-	// перевернём вектор задом наперёд
-	const auto start_time1 = steady_clock::now();
-	vector<int> reversed_bits = ReverseVector(random_bits);
-	const auto end_time1 = steady_clock::now();
-	const auto dur1 = end_time1 - start_time1;
-	cerr << "Reverse: "s << chrono::duration_cast<chrono::milliseconds>(dur1).count() << " ms"s << endl;
-
-
-	const auto start_time2 = steady_clock::now();
-	// посчитаем процент единиц на начальных отрезках вектора
-	for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
-		// чтобы вычислить проценты, мы умножаем на литерал 100. типа double;
-		// целочисленное значение функции CountPops при этом автоматически
-		// преобразуется к double, как и i
-		 double rate = CountPops(reversed_bits, 0, i) * 100. / i;
-		cout << "After "s << i << " bits we found "s << rate << "% pops"s<< endl;
+	{
+		LogDuration sleep_guard("Fill random"s);
+		FillRandom(random_bits, N);
 	}
-	const auto end_time2 = steady_clock::now();
-	const auto dur2 = end_time2 - start_time2;
-	cerr << "Counting: "s << chrono::duration_cast<chrono::milliseconds>(dur2).count() << " ms"s << endl;
+
+	vector<int> reversed_bits;
+	{
+		LogDuration sleep_guard("Reverse"s);
+		reversed_bits = ReverseVector(random_bits);
+	}
+
+	{
+		LogDuration sleep_guard("Counting"s);
+		for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
+			double rate = CountPops(reversed_bits, 0, i) * 100. / i;
+			cout << "After "s << i << " bits we found "s << rate << "% pops"s
+					<< endl;
+		}
+	}
 }
 
 int main() {
