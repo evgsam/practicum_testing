e8a3e6ad73c97246ed3d45060726ac36dd362bcc
user <user@samarin.ppsrk.ru>
2022-01-11 14:06:29 +0300

    измеряем и ускоряем

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -11,7 +11,35 @@ vector<int> ReverseVector(const vector<int> &source_vector) {
 	for (int i : source_vector) {
 		res.insert(res.begin(), i);
 	}
+	return res;
+}
 
+vector<int> ReverseVector2(const vector<int> &source_vector) {
+	vector<int> res;
+	for (auto iterator = source_vector.rbegin();
+			iterator != source_vector.rend(); ++iterator) {
+		res.push_back(*iterator);
+	}
+	return res;
+}
+
+vector<int> ReverseVector3(const vector<int> &source_vector) {
+	vector<int> res;
+	res.reserve(source_vector.size());
+	for (auto iterator = source_vector.rbegin();
+			iterator != source_vector.rend(); ++iterator) {
+		res.push_back(*iterator);
+	}
+	return res;
+}
+
+vector<int> ReverseVector4(const vector<int> &source_vector) {
+	vector<int> res;
+	res.reserve(source_vector.size());
+	for (auto iterator = source_vector.rbegin();
+			iterator != source_vector.rend(); ++iterator) {
+		res.push_back(*iterator);
+	}
 	return res;
 }
 
@@ -33,29 +61,40 @@ void AppendRandom(vector<int> &v, int n) {
 
 void Operate() {
 	vector<int> random_bits;
-
-	LOG_DURATION("Total"s);
-	//LogDuration ld("Total"s);
-	static const int N = 1 << 17;
+    uint64_t vect_size = 0;
+	cin >> vect_size;
+    
+	//static const int N = vect_size;
 	{
-		LOG_DURATION("Append random"s);
-		AppendRandom(random_bits, N);
+		AppendRandom(random_bits, vect_size);
 	}
 
 	vector<int> reversed_bits;
-	{
-		LOG_DURATION("Reverse"s);
-		reversed_bits = ReverseVector(random_bits);
-	}
-
-	{
-		LOG_DURATION("Counting"s);
-		for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
-			double rate = CountPops(reversed_bits, 0, i) * 100. / i;
-			cout << "After "s << i << " bits we found "s << rate << "% pops"s
-					<< endl;
+	if (vect_size <= 100000) {
+		{
+			LOG_DURATION("Naive"s);
+			reversed_bits = ReverseVector(random_bits);
+		}
+		{
+			LOG_DURATION("Good"s);
+			reversed_bits = ReverseVector2(random_bits);
 		}
 	}
+	if (vect_size > 100000) {
+		{
+			LOG_DURATION("Good"s);
+			reversed_bits = ReverseVector2(random_bits);
+		}
+		{
+			LOG_DURATION("Best"s);
+			reversed_bits = ReverseVector3(random_bits);
+		}
+		{
+			LOG_DURATION("Your"s);
+			reversed_bits = ReverseVector4(random_bits);
+		}
+	}
+
 }
 
 int main() {
