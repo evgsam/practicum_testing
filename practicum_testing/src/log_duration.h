6589f056b2813c605444ced1dfd4390e14d9c929
evgsam <evgsamarin@gmail.com>
2022-01-12 20:48:35 +0300

    внес макросы

--- practicum_testing/src/log_duration.h
+++ practicum_testing/src/log_duration.h
@@ -3,13 +3,18 @@
 #include <chrono>
 #include <iostream>
 
+#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+
 class LogDuration {
 public:
     // заменим имя типа std::chrono::steady_clock
     // с помощью using для удобства
     using Clock = std::chrono::steady_clock;
 
-    LogDuration(std::string functrionName): FN(functrionName){
+    LogDuration(const std::string& id) : id_(id) {
     }
 
     ~LogDuration() {
@@ -18,10 +23,10 @@ public:
 
         const auto end_time = Clock::now();
         const auto dur = end_time - start_time_;
-        std::cerr << FN << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+        std::cerr << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
     }
 
 private:
+    const std::string id_;
     const Clock::time_point start_time_ = Clock::now();
-    std::string FN;
 };
