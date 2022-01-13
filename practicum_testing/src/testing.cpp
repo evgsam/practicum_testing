09483a1699e5accf4a8f591958741f43c882d19b
user <user@samarin.ppsrk.ru>
2022-01-13 15:57:46 +0300

    вглубь процессора 2

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -8,35 +8,54 @@
 
 using namespace std;
 
-vector<float> ComputeAvgTemp(const vector<vector<float>>& vs) {
-    // место для вашего решения
+vector<float> ComputeAvgTemp(const vector<vector<float>> &vs) {
+	int m = vs.size();
+	int n = vs[0].size();
+	vector<float> result(n);
+	vector<float> sum(n);
+	vector<float> size(n);
+
+	// теперь первым идёт цикл по j
+	for (int i = 0; i < n; ++i) {
+		sum = { };
+		size = { };
+
+		for (int j = 0; j < m; ++j) {
+			sum[i] += (vs[j][i] > 0 ? vs[j][i] : 0);
+			size[i] += (vs[j][i] > 0 ? 1 : 0);
+		}
+		result[i] += (size[i] > 0 ? sum[i] / size[i] : 0);
+	}
+	return result;
+
 }
 
 vector<float> GetRandomVector(int size) {
-    static mt19937 engine;
-    uniform_real_distribution<float> d(-100, 100);
+	static mt19937 engine;
+	uniform_real_distribution<float> d(-100, 100);
 
-    vector<float> res(size);
-    for (int i = 0; i < size; ++i) {
-        res[i] = d(engine);
-    }
+	vector<float> res(size);
+	for (int i = 0; i < size; ++i) {
+		res[i] = d(engine);
+	}
 
-    return res;
+	return res;
 }
 
 int main() {
-    vector<vector<float>> data;
-    data.reserve(5000);
+	vector<vector<float>> data;
+	data.reserve(10);
 
-    for (int i = 0; i < 5000; ++i) {
-        data.push_back(GetRandomVector(5000));
-    }
+	for (int i = 0; i < 5; ++i) {
+		data.push_back(GetRandomVector(5));
+	}
 
-    vector<float> avg;
-    {
-        LOG_DURATION("ComputeAvgTemp"s);
-        avg = ComputeAvgTemp(data);
-    }
+	vector<float> avg;
+	{
+		LOG_DURATION("ComputeAvgTemp"s);
+		avg = ComputeAvgTemp(data);
+	}
 
-    cout << "Total mean: "s << accumulate(avg.begin(), avg.end(), 0.f) / avg.size() << endl;
+	cout << "Total mean: "s
+			<< accumulate(avg.begin(), avg.end(), 0.f) / avg.size() << endl;
 }
