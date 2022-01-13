2b7dcc1bbbd2ea5c8978d768539116b53951a732
evgsam <evgsamarin@gmail.com>
2022-01-13 20:07:33 +0300

    связь потоков

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,16 +1,38 @@
-#include <fstream>
+#include <iostream>
 #include <string>
+
 #include "log_duration.h"
 
 using namespace std;
 
-int main() {
-    ofstream out_file("ballad.txt"s);
-    {
-    	LOG_DURATION("Text write"s);
-    for (int i = 0; i < 50000; ++i) {
-    	out_file << "С любимыми не расставайтесь"<<endl;
+// main с аргументами означает, что программа принимает аргументы командной строки,
+// при запуске из консоли их количество будет сохранено в переменную argc,
+// а значения — в argv.
+// Тип const char** будет обсуждаться позже в курсе, работать с ним можно
+// отчасти как с вектором: argv[0] — нулевой аргумент, argv[1] — первый.
+// argv[i] можно конвертировать в string
+int main(int argc, const char** argv) {
+    // Нулевой аргумент — это всегда имя программы,
+    // поэтому нам нужен первый
+    int arg = stoi(argv[1]);
+
+    if (arg == 1) {
+        LOG_DURATION("endl"s);
+        int i;
+        while (cin >> i) {
+            cout << i * i << endl;
+        }
     }
+
+    if (arg == 2) {
+        // Чтобы "\n" воспринималось не как перевод строки, а как
+        // слеш и буква 'n', добавим перед ним ещё один слеш:
+        // два слеша в строковом (и символьном) литерале воспринимаются
+        // как один (\)
+        LOG_DURATION("\\n"s);
+        int i;
+        while (cin >> i) {
+            cout << i * i << "\n"s;
+        }
     }
-    throw;
 }
