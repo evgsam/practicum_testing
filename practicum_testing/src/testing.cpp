e719cac6d9795b52ab7608931c986e83e15341af
user <user@samarin.ppsrk.ru>
2022-01-14 14:59:13 +0300

    потоки

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -6,21 +6,27 @@ using namespace std;
 
 class StreamUntier {
 public:
-    // добавьте конструктор, деструктор
-    // и дополнительные поля класса при необходимости
+	StreamUntier(istream &tied_before){
+		tied_before_ = cin.tie(nullptr);
+	}
+	~StreamUntier(){
+		cin.tie(tied_before_);
+	}
+	// добавьте конструктор, деструктор
+	// и дополнительные поля класса при необходимости
 
 private:
-    ostream* tied_before_;
+	ostream *tied_before_;
 };
 
 int main() {
-    LOG_DURATION("\\n with tie"s);
+	LOG_DURATION("\\n with tie"s);
 
-    StreamUntier guard(cin);
-    int i;
-    while (cin >> i) {
-        cout << i * i << "\n"s;
-    }
+	StreamUntier guard(cin);
+	int i;
+	while (cin >> i) {
+		cout << i * i << "\n"s;
+	}
 
-    return 0;
+	return 0;
 }
