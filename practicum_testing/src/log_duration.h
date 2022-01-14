70e9ab25cee3396923627da80bcb276cc157bd97
user <user@samarin.ppsrk.ru>
2022-01-14 07:57:32 +0300

    связь потоков

--- practicum_testing/src/log_duration.h
+++ practicum_testing/src/log_duration.h
@@ -3,6 +3,10 @@
 #include <chrono>
 #include <iostream>
 
+using namespace std;
+using namespace chrono;
+using namespace literals;
+
 #define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
 #define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
 #define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
@@ -10,23 +14,16 @@
 
 class LogDuration {
 public:
-    // заменим имя типа std::chrono::steady_clock
-    // с помощью using для удобства
-    using Clock = std::chrono::steady_clock;
-
     LogDuration(const std::string& id) : id_(id) {
     }
 
     ~LogDuration() {
-        using namespace std::chrono;
-        using namespace std::literals;
-
-        const auto end_time = Clock::now();
+        const auto end_time = steady_clock::now();
         const auto dur = end_time - start_time_;
-        std::cerr << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+        cerr << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << endl;
     }
 
 private:
     const std::string id_;
-    const Clock::time_point start_time_ = Clock::now();
+    const steady_clock::time_point start_time_ = steady_clock::now();
 };
--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,42 +1,26 @@
-#include <iostream>
-#include <string>
-
 #include "log_duration.h"
+
+#include <iostream>
 
 using namespace std;
 
-int main(int argc, const char** argv) {
-    // не забываем, что один аргумент — это название программы,
-    // поэтому argc должно быть как минимум 2
-    if (argc < 2) {
-        cerr << "Пожалуйста, задайте как минимум 1 аргумент"s << endl;
-        return 1;
+class StreamUntier {
+public:
+    // добавьте конструктор, деструктор
+    // и дополнительные поля класса при необходимости
+
+private:
+    ostream* tied_before_;
+};
+
+int main() {
+    LOG_DURATION("\\n with tie"s);
+
+    StreamUntier guard(cin);
+    int i;
+    while (cin >> i) {
+        cout << i * i << "\n"s;
     }
 
-    int arg = std::stoi(argv[1]);
-
-    if (arg == 1) {
-        LOG_DURATION("endl"s);
-        int i;
-        while (cin >> i) {
-            cout << i * i << endl;
-        }
-    }
-
-    if (arg == 2) {
-        LOG_DURATION("\\n"s);
-        int i;
-        while (cin >> i) {
-            cout << i * i << "\n"s;
-        }
-    }
-
-    if (arg == 3) {
-        LOG_DURATION("\\n with tie"s);
-        cin.tie(nullptr);
-        int i;
-        while (cin >> i) {
-            cout << i * i << "\n"s;
-        }
-    }
+    return 0;
 }
