5b0d3adb55f6e687ebb20a6ac4ef70269828950c
evgsam <evgsamarin@gmail.com>
2022-01-16 20:22:37 +0300

    опасности экспоненты. решение через цикл

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -3,15 +3,47 @@
 
 using namespace std;
 
-template <typename F>
+template<typename F>
+string BruteForceInternal(F check, const string &begin, int n) {
+	if (n == 5) {
+		if (check(begin)) {
+			return begin;
+		} else {
+			return ""s;
+		}
+	}
+
+}
+
+template<typename F>
 string BruteForce(F check) {
-    // верните строку str, для которой check(str) будет true
+	string current_pas="AAAAA"s;
+	string result=""s;
+	for (char a = 'A'; a <= 'Z'; a++) {
+		current_pas[0]=a;
+		for (char b = 'A'; b <= 'Z'; b++) {
+			current_pas[1]=b;
+			for (char c = 'A'; c <= 'Z'; c++) {
+				current_pas[2]=c;
+				for (char d = 'A'; d <= 'Z'; d++) {
+					current_pas[3]=d;
+					for (char e = 'A'; e <= 'Z'; e++) {
+						current_pas[4]=e;
+						if (check(current_pas)) {
+							result=current_pas;
+						}
+					}
+				}
+			}
+		}
+	}
+	return result;
 }
 
 int main() {
-    string pass = "ARTUR"s;
-    auto check = [pass](const string& s) {
-        return s == pass;
-    };
-    cout << BruteForce(check) << endl;
+	string pass = "ARTUR"s;
+	auto check = [pass](const string &s) {
+		return s == pass;
+	};
+	cout << BruteForce(check) << endl;
 }
