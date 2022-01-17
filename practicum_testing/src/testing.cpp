5512321e352f27c5177e28ea59163f269700e038
user <user@samarin.ppsrk.ru>
2022-01-17 16:02:27 +0300

    амортизированная сложность

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -13,17 +13,23 @@ class TicketOffice {
 public:
     // добавить билет в систему
     void PushTicket(const string& name) {
+    	ticket_.id=last_id_;
+    	ticket_.name=name;
+    	tickets_.push_back(ticket_);
+    	++last_id_;
         // реализуйте метод
     }
 
     // получить количество доступных билетов
     int GetAvailable() const {
-        // реализуйте метод
+        return tickets_.size();
+        		// реализуйте метод
     }
 
     // получить количество доступных билетов определённого типа
     int GetAvailable(const string& name) const {
-        // реализуйте метод
+
+    	// реализуйте метод
     }
 
     // отозвать старые билеты (до определённого id)
@@ -33,5 +39,12 @@ public:
 
 private:
     int last_id_ = 0;
+    Ticket ticket_;
     deque<Ticket> tickets_;
+
 };
+
+int main(){
+	TicketOffice TO;
+	return 0;
+}
