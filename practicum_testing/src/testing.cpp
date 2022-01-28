321029e4a51543fcf99dd0f30fb57d712c53a0d8
evgsam <evgsamarin@gmail.com>
2022-01-28 20:26:30 +0300

    копирование объектов.часть 3. заготовки

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -1,100 +1,109 @@
+#include <algorithm>
 #include <cassert>
-#include <stdexcept>
+#include <vector>
 
-using namespace std::string_literals;
+using namespace std;
 
-template<typename T>
-class ScopedPtr {
+template <typename T>
+class PtrVector {
 public:
-	ScopedPtr() = default;
-	explicit ScopedPtr(T *raw_ptr) noexcept :
-			ptr_(raw_ptr) {
-	}
-	ScopedPtr(const ScopedPtr&) = delete;
-	~ScopedPtr() {
-		delete ptr_;
-	}
+    PtrVector() = default;
 
-	T* GetRawPtr() const noexcept {
-		return ptr_;
-	}
-	T* Release() noexcept {
-		T *ptr2_ = ptr_;
-		ptr_ = nullptr;
-		return ptr2_;
-	}
-		
-    // Оператор приведения к типу bool позволяет узнать, ссылается ли умный указатель
-    // на какой-либо объект
-    explicit operator bool() const noexcept {
-        if (ptr_==nullptr){
-        	return false;
-        }
-        else {
-        	return true;
-		}
+    // Создаёт вектор указателей на копии объектов из other
+    PtrVector(const PtrVector& other) {
+        // Реализуйте копирующий конструктор самостоятельно
     }
 
-    // Оператор разыменования возвращает ссылку на объект
-    // Выбрасывает исключение std::logic_error, если указатель нулевой
-    T& operator*() const {
-    	if (ptr_==nullptr){
-    		throw std::logic_error("ptr == nullptr");
-    	}
-    	else {
-			return &ptr_;
-		}
-
-        // Реализуйте самостоятельно
+    // Деструктор удаляет объекты в куче, на которые ссылаются указатели,
+    // в векторе items_
+    ~PtrVector() {
+        // Реализуйте тело деструктора самостоятельно
     }
 
-    // Оператор -> должен возвращать указатель на объект
-    // Выбрасывает исключение std::logic_error, если указатель нулевой
-    T* operator->() const {
-    	if (ptr_==nullptr){
-    		throw std::logic_error("ptr == nullptr");
-    	}
-    	else {
-			return *ptr_;
-		}
-    	// Реализуйте самостоятельно
+    // Возвращает ссылку на вектор указателей
+    vector<T*>& GetItems() noexcept {
+        // Реализуйте метод самостоятельно
     }
 
+    // Возвращает константную ссылку на вектор указателей
+    vector<T*> const& GetItems() const noexcept {
+        // Реализуйте метод самостоятельно
+    }
 
 private:
-	T *ptr_ = nullptr;
+    vector<T*> items_;
 };
 
+// Эта функция main тестирует шаблон класса PtrVector
 int main() {
-	struct DeletionSpy {
-		explicit DeletionSpy(bool &is_deleted) :
-				is_deleted_(is_deleted) {
-		}
-		~DeletionSpy() {
-			is_deleted_ = true;
-		}
-		bool &is_deleted_;
-	};
-	{
-		bool is_deleted = false;
-		{
-			DeletionSpy *raw_ptr = new DeletionSpy(is_deleted);
-			ScopedPtr<DeletionSpy> p(raw_ptr);
-			assert(p.GetRawPtr() == raw_ptr);
-			assert(!is_deleted);
-		}
-		assert(is_deleted);
-	}
-	{
-		bool is_deleted = false;
-		DeletionSpy *raw_ptr = new DeletionSpy(is_deleted);
-		{
-			ScopedPtr<DeletionSpy> scoped_ptr(raw_ptr);
-			assert(scoped_ptr.Release() == raw_ptr);
-			assert(scoped_ptr.GetRawPtr() == nullptr);
-		}
-		assert(!is_deleted);
-		delete raw_ptr;
-		assert(is_deleted);
-	}
+    // Вспомогательный "шпион", позволяющий узнать о своём удалении
+    struct DeletionSpy {
+        explicit DeletionSpy(bool& is_deleted)
+            : is_deleted_(is_deleted) {
+        }
+        ~DeletionSpy() {
+            is_deleted_ = true;
+        }
+        bool& is_deleted_;
+    };
+
+    // Проверяем удаление элементов
+    {
+        bool spy1_is_deleted = false;
+        DeletionSpy* ptr1 = new DeletionSpy(spy1_is_deleted);
+        {
+            PtrVector<DeletionSpy> ptr_vector;
+            ptr_vector.GetItems().push_back(ptr1);
+            assert(!spy1_is_deleted);
+
+            // Константная ссылка на ptr_vector
+            const auto& const_ptr_vector_ref(ptr_vector);
+            // И константная, и неконстантная версия GetItems
+            // должны вернуть ссылку на один и тот же вектор
+            assert(&const_ptr_vector_ref.GetItems() == &ptr_vector.GetItems());
+        }
+        // При разрушении ptr_vector должен удалить все объекты, на которые
+        // ссылаются находящиеся внутри него указателели
+        assert(spy1_is_deleted);
+    }
+
+    // Вспомогательный «шпион», позволяющий узнать о своём копировании
+    struct CopyingSpy {
+        explicit CopyingSpy(int& copy_count)
+            : copy_count_(copy_count) {
+        }
+        CopyingSpy(const CopyingSpy& rhs)
+            : copy_count_(rhs.copy_count_)  //
+        {
+            ++copy_count_;
+        }
+        int& copy_count_;
+    };
+
+    // Проверяем копирование элементов при копировании массива указателей
+    {
+        // 10 элементов
+        vector<int> copy_counters(10);
+
+        PtrVector<CopyingSpy> ptr_vector;
+        // Подготавливаем оригинальный массив указателей
+        for (auto& counter : copy_counters) {
+            ptr_vector.GetItems().push_back(new CopyingSpy(counter));
+        }
+        // Последний элемент содержит нулевой указатель
+        ptr_vector.GetItems().push_back(nullptr);
+
+        auto ptr_vector_copy(ptr_vector);
+        // Количество элементов в копии равно количеству элементов оригинального вектора
+        assert(ptr_vector_copy.GetItems().size() == ptr_vector.GetItems().size());
+
+        // копия должна хранить указатели на новые объекты
+        assert(ptr_vector_copy.GetItems() != ptr_vector.GetItems());
+        // Последний элемент исходного массива и его копии - нулевой указатель
+        assert(ptr_vector_copy.GetItems().back() == nullptr);
+        // Проверяем, что элементы были скопированы (копирующие шпионы увеличивают счётчики копий).
+        assert(all_of(copy_counters.begin(), copy_counters.end(), [](int counter) {
+            return counter == 1;
+        }));
+    }
 }
