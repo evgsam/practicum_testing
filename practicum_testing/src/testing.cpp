f1766aadcee656d09f0913085882f8c9ec3ca290
evgsam <evgsamarin@gmail.com>
2022-01-29 17:07:38 +0300

    код для теста

--- practicum_testing/src/testing.cpp
+++ practicum_testing/src/testing.cpp
@@ -56,143 +56,89 @@ private:
 ;
 
 // Эта функция тестирует работу SingleLinkedList
-void Test1() {
-	// Шпион, следящий за своим удалением
-	struct DeletionSpy {
-		DeletionSpy() = default;
-		explicit DeletionSpy(int &instance_counter) noexcept :
-				instance_counter_ptr_(&instance_counter)  //
-		{
-			OnAddInstance();
-		}
-		DeletionSpy(const DeletionSpy &other) noexcept :
-				instance_counter_ptr_(other.instance_counter_ptr_)  //
-		{
-			OnAddInstance();
-		}
-		DeletionSpy& operator=(const DeletionSpy &rhs) noexcept {
-			if (this != &rhs) {
-				auto rhs_copy(rhs);
-				std::swap(instance_counter_ptr_,
-						rhs_copy.instance_counter_ptr_);
-			}
-			return *this;
-		}
-		~DeletionSpy() {
-			OnDeleteInstance();
-		}
+void Test2() {
+    // Итерирование по пустому списку
+    {
+        SingleLinkedList<int> list;
+        // Константная ссылка для доступа к константным версиям begin()/end()
+        const auto& const_list = list;
 
-	private:
-		void OnAddInstance() noexcept {
-			if (instance_counter_ptr_) {
-				++(*instance_counter_ptr_);
-			}
-		}
-		void OnDeleteInstance() noexcept {
-			if (instance_counter_ptr_) {
-				assert(*instance_counter_ptr_ != 0);
-				--(*instance_counter_ptr_);
-			}
-		}
+        // Итераторы begine и end у пустого диапазона равны друг другу
+        assert(list.begin() == list.end());
+        assert(const_list.begin() == const_list.end());
+        assert(list.cbegin() == list.cend());
+        assert(list.cbegin() == const_list.begin());
+        assert(list.cend() == const_list.end());
+    }
 
-		int *instance_counter_ptr_ = nullptr;
-	};
+    // Итерирование по непустому списку
+    {
+        SingleLinkedList<int> list;
+        const auto& const_list = list;
 
-	// Проверка вставки в начало
-	{
-		SingleLinkedList<int> l;
-		assert(l.IsEmpty());
-		assert(l.GetSize() == 0u);
+        list.PushFront(1);
+        assert(list.GetSize() == 1u);
+        assert(!list.IsEmpty());
 
-		l.PushFront(0);
-		l.PushFront(1);
-		assert(l.GetSize() == 2);
-		assert(!l.IsEmpty());
+        assert(const_list.begin() != const_list.end());
+        assert(const_list.cbegin() != const_list.cend());
+        assert(list.begin() != list.end());
 
-		l.Clear();
-		assert(l.GetSize() == 0);
-		assert(l.IsEmpty());
-	}
+        assert(const_list.begin() == const_list.cbegin());
 
-	// Проверка фактического удаления элементов
-	{
-		int item0_counter = 0;
-		int item1_counter = 0;
-		int item2_counter = 0;
-		{
-			SingleLinkedList<DeletionSpy> list;
-			list.PushFront(DeletionSpy { item0_counter });
-			list.PushFront(DeletionSpy { item1_counter });
-			list.PushFront(DeletionSpy { item2_counter });
+        assert(*list.cbegin() == 1);
+        *list.begin() = -1;
+        assert(*list.cbegin() == -1);
 
-			assert(item0_counter == 1);
-			assert(item1_counter == 1);
-			assert(item2_counter == 1);
-			list.Clear();
-			assert(item0_counter == 0);
-			assert(item1_counter == 0);
-			assert(item2_counter == 0);
+        const auto old_begin = list.cbegin();
+        list.PushFront(2);
+        assert(list.GetSize() == 2);
 
-			list.PushFront(DeletionSpy { item0_counter });
-			list.PushFront(DeletionSpy { item1_counter });
-			list.PushFront(DeletionSpy { item2_counter });
-			assert(item0_counter == 1);
-			assert(item1_counter == 1);
-			assert(item2_counter == 1);
-		}
-		assert(item0_counter == 0);
-		assert(item1_counter == 0);
-		assert(item2_counter == 0);
-	}
+        const auto new_begin = list.cbegin();
+        assert(new_begin != old_begin);
+        // Проверка прединкремента
+        {
+            auto new_begin_copy(new_begin);
+            assert((++(new_begin_copy)) == old_begin);
+        }
+        // Проверка постинкремента
+        {
+            auto new_begin_copy(new_begin);
+            assert(((new_begin_copy)++) == new_begin);
+            assert(new_begin_copy == old_begin);
+        }
+        // Итератор, указывающий на позицию после последнего элемента равен итератору end()
+        {
+            auto old_begin_copy(old_begin);
+            assert((++old_begin_copy) == list.end());
+        }
+    }
+    // Преобразование итераторов
+    {
+        SingleLinkedList<int> list;
+        list.PushFront(1);
+        // Конструирование ConstItrator из Iterator
+        SingleLinkedList<int>::ConstIterator const_it(list.begin());
+        assert(const_it == list.cbegin());
+        assert(*const_it == *list.cbegin());
 
-	// Вспомогательный класс, бросающий исключение после создания N-копии
-	struct ThrowOnCopy {
-		ThrowOnCopy() = default;
-		explicit ThrowOnCopy(int &copy_counter) noexcept :
-				countdown_ptr(&copy_counter) {
-		}
-		ThrowOnCopy(const ThrowOnCopy &other) :
-				countdown_ptr(other.countdown_ptr)  //
-		{
-			if (countdown_ptr) {
-				if (*countdown_ptr == 0) {
-					throw std::bad_alloc();
-				} else {
-					--(*countdown_ptr);
-				}
-			}
-		}
-		// Присваивание элементов этого типа не требуется
-		ThrowOnCopy& operator=(const ThrowOnCopy &rhs) = delete;
-		// Адрес счётчика обратного отсчёта. Если не равен nullptr, то уменьшается при каждом копировании.
-		// Как только обнулится, конструктор копирования выбросит исключение
-		int *countdown_ptr = nullptr;
-	};
+        SingleLinkedList<int>::ConstIterator const_it1;
+        // Присваивание ConstIterator-у значения Iterator
+        const_it1 = list.begin();
+        assert(const_it1 == const_it);
+    }
+    // Проверка оператора ->
+    {
+        using namespace std;
+        SingleLinkedList<std::string> string_list;
 
-	{
-		bool exception_was_thrown = false;
-		// Последовательно уменьшаем счётчик копирований до нуля, пока не будет выброшено исключение
-		for (int max_copy_counter = 5; max_copy_counter >= 0;
-				--max_copy_counter) {
-			// Создаём непустой список
-			SingleLinkedList<ThrowOnCopy> list;
-			list.PushFront(ThrowOnCopy { });
-			try {
-				int copy_counter = max_copy_counter;
-				list.PushFront(ThrowOnCopy(copy_counter));
-				// Если метод не выбросил исключение, список должен перейти в новое состояние
-				assert(list.GetSize() == 2);
-			} catch (const std::bad_alloc&) {
-				exception_was_thrown = true;
-				// После выбрасывания исключения состояние списка должно остаться прежним
-				assert(list.GetSize() == 1);
-				break;
-			}
-		}
-		assert(exception_was_thrown);
-	}
+        string_list.PushFront("one"s);
+        assert(string_list.cbegin()->length() == 3u);
+        string_list.begin()->push_back('!');
+        assert(*string_list.begin() == "one!"s);
+    }
 }
 
 int main() {
-	Test1();
+    Test2();
 }
